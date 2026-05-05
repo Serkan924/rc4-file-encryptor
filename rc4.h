b946bcc79cccd29d0941d7681534c5080402653a
Serkan924 <serkandilber12@gmail.com>
2026-05-05 19:54:35 +0300

    Update rc4.h

--- rc4.h
+++ rc4.h
@@ -1,1 +1,9 @@
+#ifndef RC4_H
+#define RC4_H
 
+#include <stddef.h>
+
+void rc4_init(unsigned char *S, const unsigned char *key, size_t keylen);
+void rc4_crypt(unsigned char *S, unsigned char *data, size_t len);
+
+#endif
