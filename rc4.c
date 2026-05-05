292883ed61331a1f030eedf67c005760523a078f
Serkan924 <serkandilber12@gmail.com>
2026-05-05 19:55:25 +0300

    Update rc4.c

--- rc4.c
+++ rc4.c
@@ -1,1 +1,31 @@
+#include "rc4.h"
 
+void rc4_init(unsigned char *S, const unsigned char *key, size_t keylen) {
+    for (int i = 0; i < 256; i++)
+        S[i] = i;
+
+    int j = 0;
+    for (int i = 0; i < 256; i++) {
+        j = (j + S[i] + key[i % keylen]) % 256;
+
+        unsigned char temp = S[i];
+        S[i] = S[j];
+        S[j] = temp;
+    }
+}
+
+void rc4_crypt(unsigned char *S, unsigned char *data, size_t len) {
+    int i = 0, j = 0;
+
+    for (size_t k = 0; k < len; k++) {
+        i = (i + 1) % 256;
+        j = (j + S[i]) % 256;
+
+        unsigned char temp = S[i];
+        S[i] = S[j];
+        S[j] = temp;
+
+        unsigned char rnd = S[(S[i] + S[j]) % 256];
+        data[k] ^= rnd;
+    }
+}
